74687be9613516d2fe2700fc849095cefad2a448
GonzaloGuzmanDEV <ITGonzaloGuzman1997@outlook.com>
2023-03-21 21:35:14 -0300

    Sintaxis

---
+++ script.js
@@ -0,0 +1,11 @@
+let elementos = [
+
+    nombre ="Gonzalo",
+    edad = 25,
+    desarrollador = true,
+    nacimiento = Date(13/09/97),
+    libro = URL("https://www.amazon.com/-/es/Viktor-Emil-Frankl/dp/8425432022")
+
+];
+
+console.log(elementos);
